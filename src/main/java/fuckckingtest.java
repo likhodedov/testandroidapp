473f9a7591d37a830e1aea4d6a103da6ea316cac
d.lihodedov <likhodedovdmitry@gmail.com>
2016-11-03 13:35:44 +0700

    last version

--- src/main/java/fuckckingtest.java
+++ src/main/java/fuckckingtest.java
@@ -2,146 +2,65 @@ import io.selendroid.client.SelendroidDriver;
 import io.selendroid.client.TouchAction;
 import io.selendroid.client.TouchActionBuilder;
 import io.selendroid.common.SelendroidCapabilities;
-import io.selendroid.common.device.DeviceTargetPlatform;
-
 import io.selendroid.standalone.SelendroidConfiguration;
 import io.selendroid.standalone.SelendroidLauncher;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
 import org.openqa.selenium.*;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.interactions.TouchScreen;
-import org.openqa.selenium.interactions.touch.TouchActions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by d.lihodedov on 18.10.2016.
  */
 public class fuckckingtest {
-   public WebDriver driver;
 
-    @Before
-    public void setup() throws Exception {
 
-    }
+    public static void main(String args[]) throws Exception{
+        WebDriver driver;
+        String current = new java.io.File( "." ).getCanonicalPath();
+        System.out.println("Current dir:"+current);
+        SelendroidConfiguration config = new SelendroidConfiguration();
+        //Add the selendroid-test-app to the standalone server
+        config.addSupportedApp("C:\\Users\\d.lihodedov\\IdeaProjects\\testandroidapp\\src\\main\\resources\\fortest.apk");
 
-    @Test
-    public void test1() throws Exception {
-       //--------------------------------------
-        // SelendroidConfiguration config = new SelendroidConfiguration();
-// Add the selendroid-test-app to the standalone server
-       // config.addSupportedApp("src/main/resources/develop.apk");
-       // SelendroidLauncher selendroidServer = new SelendroidLauncher(config);
-       // selendroidServer.launchSelendroid();
+        SelendroidLauncher selendroidServer = new SelendroidLauncher(config);
+        selendroidServer.launchSelendroid();
         //-------------------------------------
         SelendroidCapabilities caps = new SelendroidCapabilities("com.movavi.mobile.videoeditor:1.0.4");
-        //DesiredCapabilities caps=SelendroidCapabilities.android();
         caps.setEmulator(false);
-        caps.setSerial("044a6eeff0cca631"); // NEXUS 5 (D.LIKHODEDOV)
 
+        caps.setSerial("8e34c2ad");
         driver = new SelendroidDriver(caps);
-        //caps.setCapability("app-activity", "com.movavi.mobile.videoeditor.ActivitySplash_");
         driver.get("and-activity://com.movavi.mobile.videoeditor.ActivityMain_");
-        //driver.findElement(By.id("previewImage")).click();
         WebDriverWait wait = new WebDriverWait(driver, 10000);
-        //while (driver.findElements(By.xpath("(//AppCompatImageView[@id='previewImage'])[1]")).size()<0)
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
-        //WebElement take = wait.until(ExpectedConditions.elementToBeClickable(By.id("menu_done")));
-        //WebElement hello = driver.findElement(By.id("previewImage"));
-       //builder.moveToElement(hello, 24, 528).click();
-/*if (driver.findElements(By.xpath("(//AppCompatImageView[@id='previewImage'])[1]")).size()> 0)
-{
-        //wait.until(ExpectedConditions.visibilityOf(element1));
-        WebElement element1 = driver.findElement(By.xpath("(//AppCompatImageView[@id='previewImage'])[1]"));
-        if (element1.isDisplayed()) element1.click();
-        //element1.click();
-        WebElement element2 = driver.findElement(By.xpath("(//AppCompatImageView[@id='previewImage'])[2]"));
-        if (element2.isDisplayed()) element2.click();
-
-        WebElement element3 = driver.findElement(By.xpath("(//AppCompatImageView[@id='previewImage'])[3]"));
-        if (element3.isDisplayed()) element3.click();}*/
-        checkvideoManager(3);
-
-
-
-WebElement next=driver.findElement(By.id("menu_done"));
-      next.click();
-      /*   WebElement YOUR_MUSIC_CHECK=driver.findElement(By.id("button_your_music"));
-        YOUR_MUSIC_CHECK.click();
-
-       WebElement element1 = driver.findElement(By.xpath("(//RelativeLayout[@id='audioItemWrapper'])[1]"));
-        element1.click();
-        WebElement element2 = driver.findElement(By.xpath("(//RelativeLayout[@id='audioItemWrapper'])[2]"));
-        element2.click();*/
-AudioItemSelector("App music");
-
+        driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+        checkvideoManager(3,driver);
+        WebElement next=driver.findElement(By.id("menu_done"));
+        next.click();
+        AudioItemSelector("App music",driver);
         next.click();
         WebElement element3 = driver.findElement(By.id("button_done"));
         element3.click();
         WebElement element4 = driver.findElement(By.id("menu_export"));
         element4.click();
-
-      //  WebElement element5 = driver.findElement(By.id("menu_export_cancel"));
-      //  element5.click();
-
         driver.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
         WebElement taken = wait.until(ExpectedConditions.elementToBeClickable(By.id("menu_export_new")));
-
-        //element2.click();
-        //hello.click();
-        //hello.getLocation();
-        //getLocationofElem(hello);
-        //-----------
-        //WebElement element3 = driver.findElement(By.xpath("(//ProgressBar[@id='progressBar'])[1]"));
-       // element3.click();
-
-//WebElement elem[]=new WebElement[5];
-
-
-       // driver.findElement(By.id("previewImage")).click();
-
-        //take.click();
-       // WebDriverWait waits = new WebDriverWait(driver, 10);
-        //String s = new String();
-        //caps.setCapability("app-wait-activity", "com.movavi.mobile.videoeditor.ActivityMain_");
-        //WebElement tag = wait.until(ExpectedConditions.elementToBeClickable(By.id("menu_done")));
-    }
-
-    @After
-    public void end() {
         if (driver != null) {
             driver.quit();
-        }
-    }
+   }}
 
-    public void getLocationofElem(WebElement element){
-        Point point = element.getLocation();
-        int xcord = point.getX();
-        System.out.println("Element's Position from left side Is "+xcord +" pixels.");
-        int ycord = point.getY();
-        System.out.println("Element's Position from top side Is "+ycord +" pixels.");
-    }
-
-    public void checkvideoManager(int count)  {
+    public static void checkvideoManager(int count, WebDriver driver)  {
 
         WebElement elem[]=new WebElement[count];
         for (int i=0;i<count;i++) {
             if (driver.findElements(By.xpath("(//AppCompatImageView[@id='previewImage'])["+(i+1)+"]")).size()> 0) {
         elem[i]=driver.findElement(By.xpath("(//AppCompatImageView[@id='previewImage'])["+(i+1)+"]"));
-            if(elem[i].isDisplayed()) elem[i].click();
-        }
+                if(elem[i].isDisplayed()) elem[i].click();
+        } else driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         }
     }
 
-    public void AudioItemSelector(String S) {
+    public static void AudioItemSelector(String S, WebDriver driver) {
             if (S.equals("No music")){
             WebElement YOUR_CHOICE=driver.findElement(By.id("button_no_music"));
             YOUR_CHOICE.click();
@@ -156,18 +75,32 @@ AudioItemSelector("App music");
                 WebElement YOUR_CHOICE=driver.findElement(By.id("button_program_music"));
                 YOUR_CHOICE.click();
 
-            WebElement element1 = driver.findElement(By.xpath("(//RelativeLayout[@id='audioItemWrapper'])[1]"));
-            element1.click();
-                //WebElement musictimeline = driver.findElement(By.id("rangeSeekBar"));
-              //  TouchActions flick = new TouchActions(driver).flick(musictimeline, 200, 384, 1);
-
-                TouchAction ta = new TouchActionBuilder().pointerDown(48,384).
-                        pointerMove(900,384).pointerUp().build();
-                ta.perform(driver);
-               // TouchAction ta = new TouchActionBuilder().pointerDown(48,384).
-                     //  pointerMove(300,400).pointerUp().build();
-               //ta2.perform(driver);
-
-        }
+                WebElement element1 = driver.findElement(By.xpath("//RangeSeekBar[@id='rangeSeekBar']"));
+                element1.click();
+                //getLocationofElem(element1);
+                pairvalues coordinates=getLocationofElem(element1);
+            TouchAction ta = new TouchActionBuilder().pointerDown(coordinates.getXcoord(),coordinates.getYcoord()).
+            pointerMove(coordinates.getXcoord()+coordinates.getXcoord()*3,coordinates.getYcoord()).pointerUp().build();
+            ta.perform(driver);
+                driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+                     }
+    }
+    public static pairvalues getLocationofElem(WebElement element){
+        Point point = element.getLocation();
+        pairvalues Coordinates=new pairvalues(point.getX(),point.getY());
+        return Coordinates;
+    }
+    public String DeviceSelector(String NameofDevice){
+        String outputSerial=new String();
+        if (NameofDevice.equals("Nexus_5")) outputSerial="044a6eeff0cca631";
+        else if (NameofDevice.equals("Samsung_Galaxy_Tab_4")) outputSerial="61ac735d07d7de10";
+        else if (NameofDevice.equals("Huawei_MediaPad_M2")) outputSerial="36E6R15925001108";
+        //else if (NameofDevice.equals("Nexus_5X")) outputSerial="00bd9f1ce5b6b398";
+        else if (NameofDevice.equals("Lenovo_S60")) outputSerial="1376deea";
+        else if (NameofDevice.equals("Lenovo_S860")) outputSerial="error";
+        else if (NameofDevice.equals("Asus_ZenFone_5")) outputSerial="ECAZCY02W348";
+        else if (NameofDevice.equals("Asus_ZenFone2_Laser")) outputSerial="";
+        else if (NameofDevice.equals("Samsung_J5")) outputSerial="8e34c2ad";
+return outputSerial;
     }
 }
---
+++ src/main/java/pairvalues.java
@@ -0,0 +1,21 @@
+/**
+ * Created by d.lihodedov on 27.10.2016.
+ */
+public class pairvalues {
+    private final int Xcoord;
+    private final int Ycoord;
+
+    public pairvalues(int x, int y) {
+        this.Xcoord = x;
+        this.Ycoord = y;
+    }
+
+    public int getXcoord() {
+        return Xcoord;
+    }
+
+    public int getYcoord() {
+        return Ycoord;
+    }
+
+}
