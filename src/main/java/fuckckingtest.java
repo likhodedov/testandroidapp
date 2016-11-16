163a43f61e6f9539e52c6f87f53b8c56f03c9125
d.lihodedov <likhodedovdmitry@gmail.com>
2016-11-16 13:08:30 +0700

    workspace test complete

--- src/main/java/fuckckingtest.java
+++ src/main/java/fuckckingtest.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.interactions.touch.TouchActions;
 /**
  * Created by d.lihodedov on 18.10.2016.
  */
+
 public class fuckckingtest {
     WebDriver driver;
     SelendroidConfiguration config = new SelendroidConfiguration();
@@ -21,51 +22,39 @@ public class fuckckingtest {
 
     @Before
     public void initializeSelendroid() throws Exception {
-        config.addSupportedApp("C:\\test\\testapp.apk");
+        config.addSupportedApp("C:\\test\\testapp2.apk");
         SelendroidLauncher selendroidServer = new SelendroidLauncher(config);
         selendroidServer.launchSelendroid();
         caps.setEmulator(false);
         driver = new SelendroidDriver(caps);
         driver.get("and-activity://com.movavi.mobile.videoeditor.ActivityMain_");
-        caps.setSerial(DeviceSelector("Lenovo_S60"));
-
+        caps.setSerial(DeviceSelector("Samsung_Galaxy_Tab_4"));
     }
 
+
 @Test
-    public void CheckVideoTest() throws Exception{
+    public void WorkSpaceTest() throws Exception{
 
         WebDriverWait wait = new WebDriverWait(driver, 10000);
         driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
-
+//--------------ДОБАВЛЕНИЕ ВИДЕО В ПРОЕКТ-----------------------
         checkvideoManager(4,1,driver);
         WebElement next=driver.findElement(By.id("menu_done"));
         next.click();
-//   driver.navigate().back();
-//    WebElement ClickAgree=driver.findElement(By.id("button1"));
-//    ClickAgree.click();
-//
-//
-//    driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
-//    checkvideoManager(1,4,driver);
-//    next.click();
+//--------------ДОБАВЛЕНИЕ АУДИО В ПРОЕКТ-----------------------
     AudioItemSelector("App music",driver);
-        next.click();
-        WebElement element3 = driver.findElement(By.id("button_done"));
-        element3.click();
-
+    next.click();
+    WebElement element3 = driver.findElement(By.id("button_done"));
+    element3.click();
+//--------------ВОСПРОИЗВЕДЕНИЕ ВИДЕО-----------------------
     WebElement PlayVideo = driver.findElement(By.id("buttonPlay"));
-PlayVideo.click();
+    PlayVideo.click();
     WebElement Split= driver.findElement(By.id("splitButton"));
     WebElement Crop = driver.findElement(By.id("menu_crop"));
-WebElement playSeekBar=driver.findElement(By.id("playSeekBar"));
+    WebElement playSeekBar=driver.findElement(By.id("playSeekBar"));
     pairvalues bar=getLocationofElem(playSeekBar);
-
-
-
-
-
-    //---------------------------------------------------------------------
     PlayVideoStart(driver,PlayVideo);
+//--------------РАЗРЕЗКА ТРЕКА НА СЕРЕДИНЕ ТАЙМЛАЙНА,ИЗМЕНЕНИЕ СООТНОШЕНИЯ СТОРОН И ВОСПРОИЗВЕДЕНИЕ-----------------------
     TouchAction ta = new TouchActionBuilder().pointerDown(bar.getXcoord(),bar.getYcoord()).
             pointerMove(bar.getXcoord()+playSeekBar.getSize().getWidth()/2,bar.getYcoord()).pointerUp().build();
     ta.perform(driver);
@@ -73,7 +62,7 @@ WebElement playSeekBar=driver.findElement(By.id("playSeekBar"));
     Crop.click();
     PlayVideo.click();
     PlayVideoStart(driver,PlayVideo);
-    //--------------------------------------------------------------------------------------------
+//--------------РАЗРЕЗКА ТРЕКА НА 3/4 ТАЙМЛАЙНА,ИЗМЕНЕНИЕ СООТНОШЕНИЯ СТОРОН И ВОСПРОИЗВЕДЕНИЕ----------------------------
     pairvalues bar2=getLocationofElem(playSeekBar);
     TouchAction tb = new TouchActionBuilder().pointerDown(bar2.getXcoord(),bar2.getYcoord()).
             pointerMove(bar2.getXcoord()+playSeekBar.getSize().getWidth()-playSeekBar.getSize().getWidth()/4,bar2.getYcoord()).pointerUp().build();
@@ -82,22 +71,17 @@ WebElement playSeekBar=driver.findElement(By.id("playSeekBar"));
     Crop.click();
     PlayVideo.click();
     PlayVideoStart(driver,PlayVideo);
-
+//--------------ОБРЕЗКА ВИДЕО С ОБОИХ КРАЕВ-----------------------
     WebElement ChangeDurationVideoItem = driver.findElement(By.xpath("(//RangeSeekBar[@id='rangeSeekBar'])[1]"));
 pairvalues coordinatesofChangeDurationVideoItem=getLocationofElem(ChangeDurationVideoItem);
-
-
     TouchAction tj = new TouchActionBuilder().pointerDown(coordinatesofChangeDurationVideoItem.getXcoord(),coordinatesofChangeDurationVideoItem.getYcoord()).
             pointerMove(coordinatesofChangeDurationVideoItem.getXcoord()+coordinatesofChangeDurationVideoItem.getXcoord(),coordinatesofChangeDurationVideoItem.getYcoord()).pointerUp().build();
     tj.perform(driver);
     driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
     TouchAction tk = new TouchActionBuilder().pointerDown(coordinatesofChangeDurationVideoItem.getXcoord()+ChangeDurationVideoItem.getSize().getWidth()-1,coordinatesofChangeDurationVideoItem.getYcoord()).
             pointerMove(coordinatesofChangeDurationVideoItem.getXcoord()+ChangeDurationVideoItem.getSize().getWidth()-ChangeDurationVideoItem.getSize().getWidth()/4,coordinatesofChangeDurationVideoItem.getYcoord()).pointerUp().build();
     tk.perform(driver);
-
-
-
+//--------------ИЗМЕНЕНИЕ ПОЗИЦИИ ПЕРВОГО ВИДЕОТРЕКА-----------------------
     WebElement moveVideo=driver.findElement(By.id("reorder_handle"));
     pairvalues coordmoveVideo=getLocationofElem(moveVideo);
     TouchAction tp = new TouchActionBuilder().pointerDown(coordmoveVideo.getXcoord(),coordmoveVideo.getYcoord()).
@@ -105,62 +89,51 @@ pairvalues coordinatesofChangeDurationVideoItem=getLocationofElem(ChangeDuration
     tp.perform(driver);
     driver.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
 
+
+//--------------ПРОЛИСТЫВАНИЕ СПИСКА ДОБАВЛЕННЫХ ВИДЕО ДЛЯ НАЖАТИЯ КНОПКИ "+"(ДОБАВЛЕНИЕ ВИДЕО)-----------------------
     WebElement item1 = driver.findElement(By.xpath("(//LinearLayout[@id='item_layout'])[1]"));
     pairvalues item=getLocationofElem(item1);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         TouchAction tr = new TouchActionBuilder().pointerDown(item.getXcoord(),item.getYcoord()).
                 pointerMove(item.getXcoord(),0).pointerUp().build();
         tr.perform(driver);
-
+//--------------ДОБАВЛЕНИЕ ВИДЕО В ПРОЕКТ-----------------------
     WebElement AddNewVideo=driver.findElement(By.id("buttonAdd"));
     AddNewVideo.click();
     checkvideoManager(1,5,driver);
 
     WebElement element1 = driver.findElement(By.xpath("//ActionMenuItemView[@id='menu_done']"));
     element1.click();
-
+//--------------ОТКРЫТИЕ СВОЙСТВ ВИДЕО-----------------------
     WebElement itemtodelandcopy=driver.findElement(By.xpath("(//LinearLayout[@id='item_layout'])[1]"));
     pairvalues todelandcopy=getLocationofElem(itemtodelandcopy);
     TouchAction th = new TouchActionBuilder().pointerDown(todelandcopy.getXcoord(),todelandcopy.getYcoord()).
             pointerMove(todelandcopy.getXcoord()+itemtodelandcopy.getSize().getWidth(),todelandcopy.getYcoord()).pointerUp().build();
     th.perform(driver);
+//--------------КОПИРОВАНИЕ ВИДЕОДОРОЖКИ-----------------------
     WebElement buttoncopy = driver.findElement(By.xpath("(//AppCompatButton[@id='buttonCopy'])[1]"));
     buttoncopy.click();
     TouchAction tf = new TouchActionBuilder().pointerDown(todelandcopy.getXcoord(),todelandcopy.getYcoord()).
             pointerMove(todelandcopy.getXcoord()+itemtodelandcopy.getSize().getWidth(),todelandcopy.getYcoord()).pointerUp().build();
     tf.perform(driver);
+//--------------УДАЛЕНИЕ ВИДЕОДОРОЖКИ И ОТМЕНА ДЕЙСТВИЯ-----------------------
     WebElement buttonDelete = driver.findElement(By.xpath("(//AppCompatButton[@id='buttonDelete'])[1]"));
     buttonDelete.click();
 WebElement SnackBar = driver.findElement(By.id("snackbar_action"));
     SnackBar.click();
+//--------------ОТКРЫТИЕ ВКЛАДКИ С АУДИО И ВОЗВРАТ ОБРАТНО-----------------------
     driver.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
     WebElement MusicAdd=driver.findElement(By.id("menu_add_music"));
     MusicAdd.click();
     driver.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
     driver.navigate().back();
-
+//--------------ОТКРЫТИЕ FULLSCREEN И ВОСПРОИЗВЕДЕНИЕ ВСЕГО ВИДЕО-----------------------
     WebElement fullscreen=driver.findElement(By.id("fullscreen_toggle"));
     fullscreen.click();
     WebElement playfullscreen = driver.findElement(By.xpath("//AppCompatImageView[@id='buttonPlay']"));
     playfullscreen.click();
     PlayVideoStart(driver,playfullscreen);
     fullscreen.click();
-//    }
-     // wait.until(ExpectedConditions.elementToBeClickable(PlayVideo));
-
+//--------------ЭКСПОРТ И ОЖИДАНИЕ ОКОНЧАНИЯ ЭКСПОРТА-----------------------
         WebElement element4 = driver.findElement(By.id("menu_export"));
         element4.click();
         driver.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
@@ -172,14 +145,62 @@ WebElement SnackBar = driver.findElement(By.id("snackbar_action"));
 
 
     @Test
-    public void WorkspaceTest() throws Exception{
+    public void CheckVideoTest() throws Exception{
+        WebDriverWait wait = new WebDriverWait(driver, 10000);
+        driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
 
+        checkvideoManager(4,1,driver);
+        WebElement next=driver.findElement(By.id("menu_done"));
+        next.click();
+        driver.navigate().back();
+        WebElement ClickAgree=driver.findElement(By.id("button1"));
+        ClickAgree.click();
+
+
+        driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+        checkvideoManager(1,4,driver);
+        next.click();
+
+        AudioItemSelector("App music",driver);
+        next.click();
+        WebElement element3 = driver.findElement(By.id("button_done"));
+        element3.click();
+
+        WebElement PlayVideo = driver.findElement(By.id("buttonPlay"));
+        PlayVideo.click();
+        WebElement playSeekBar=driver.findElement(By.id("playSeekBar"));
+        pairvalues bar=getLocationofElem(playSeekBar);
+
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
 
     @Test
     public void AudioTest(){
+        WebDriverWait wait = new WebDriverWait(driver, 10000);
+        driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
 
+        checkvideoManager(4,1,driver);
+        WebElement next=driver.findElement(By.id("menu_done"));
+        next.click();
+        AudioItemSelector("App music",driver);
+        next.click();
+        WebElement element3 = driver.findElement(By.id("button_done"));
+        element3.click();
+        WebElement PlayVideo = driver.findElement(By.id("buttonPlay"));
+        PlayVideo.click();
+        PlayVideoStart(driver,PlayVideo);
+        driver.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
+        WebElement MusicAdd=driver.findElement(By.id("menu_add_music"));
+        MusicAdd.click();
+        AudioItemSelector("No music",driver);
+        //-----НУЖНО ДОБАВЛЕНИЕ ВЫБРАННОГО АУДИО-----
+
+        PlayVideo.click();
+        PlayVideoStart(driver,PlayVideo);
+        driver.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
     }
 
     @Test
@@ -233,7 +254,7 @@ WebElement SnackBar = driver.findElement(By.id("snackbar_action"));
             ta.perform(driver);
                 driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
-        TouchAction tb = new TouchActionBuilder().pointerDown(element1.getSize().getWidth(),coordinates.getYcoord()).
+        TouchAction tb = new TouchActionBuilder().pointerDown(coordinates.getXcoord()+element1.getSize().getWidth()-1,coordinates.getYcoord()).
                 pointerMove((coordinates.getXcoord()+element1.getSize().getWidth()-element1.getSize().getWidth()/4),coordinates.getYcoord()).pointerUp().build();
         tb.perform(driver);}
     }
